e76f04100e20f30b1177d764898377be8ad71fb2
FabianStudium <fabian.gazzia@stud.fh-campuswien.ac.at>
2023-11-21 20:24:01 +0100

    added register and login route with vulenarble login

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
 const express = require('express');
+const db = require('./database');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+app.post('/register', (req, res) => {
+    const { username, password, email } = req.body;
+    const query = `INSERT INTO users (username, password, email) VALUES (?, ?, ?)`;
+    db.run(query, [username, password, email], function(err) {
+        if (err) {
+            res.status(400).send('Error in registration');
+        } else {
+            res.status(201).send(`User created with ID: ${this.lastID}`);
+        }
+    });
+});
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+
+    // vulnerbale sql injection
+    const query = `SELECT * FROM users WHERE email = '${email}' AND password = '${password}'`;
+    db.get(query, [], (err, row) => {
+        if (err) {
+            res.status(400).send('Error in login');
+        } else if (row) {
+            res.status(200).send('Login successful');
+        } else {
+            res.status(401).send('Invalid credentials');
+        }
+    });
+});
+  
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
